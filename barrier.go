d062f86f399bae392192a3d81ccc1b54805bdab9
Mufidzz <dmzmzzz@gmail.com>
2020-12-14 21:58:17 +0700

    Fix Missing Return on barrier.go

--- barrier.go
+++ barrier.go
@@ -1,9 +1,13 @@
 package iflogs
 
 type Barrier struct {
-    URL string
+	URL string
 }
 
-func (*Barrier) UseDefault() {
-	return
+func (*Barrier) UseDefault() Engine {
+	return Engine{
+		Barrier: Barrier{
+			URL: "localhost",
+		},
+	}
 }
